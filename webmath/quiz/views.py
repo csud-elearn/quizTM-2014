01b3cebd746201fc5bab93699d83c853bb478e1b
Benoît Léo Maillard <benoitleo.maillard@gmail.com>
2015-03-11 12:46:16 +0000

    Ajout des décorateurs d'authentification dans les views

--- webmath/quiz/views.py
+++ webmath/quiz/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect, get_object_or_404
 from django.core.urlresolvers import reverse
-import json
 from quiz.utils.save import SaveQuiz
 from quiz.utils.submit import QuizForms
 from quiz.utils.correct import CorrectQuiz
 from quiz.forms import TextForm, CheckboxForm, RadioForm, SelectForm
 from quiz.models import Quiz, QuizDraft, CompletedQuiz, SqSubmit, QcmSubmitMulti, QcmSubmitOne
-
-# Create your views here.
+from common.auth_utils import *
+from django.contrib.auth.decorators import login_required, user_passes_test
+import json
 
 def index(request):
+    print("utilisateur : " + str(request.user))
+    print(request.user.groups.all())
     return render(request, 'quiz/index.html')
 
+@login_required
+@user_passes_test(is_teacher)
 def create(request):
     if request.method == 'POST':
         json_string = request.POST['json'] #Chaîne json contenant toutes les données des questions du quiz
@@ -25,7 +29,8 @@ def create(request):
         return HttpResponseRedirect(reverse("quiz:complete", args=[quiz.get_id()]))
     else:
         return render(request, 'quiz/create.html')
-        
+
+@login_required
 def complete(request, n_quiz):
     # Si la requête est de type POST, on créer un nouveau formulaire et on le remplit avec les données de requête
     if request.method == 'POST':
